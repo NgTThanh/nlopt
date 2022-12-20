c0273607b821ac01b58df7f0febb881e18892ce1
NgTThanh <ngtuanthanh0206@gmail.com>
2022-12-20 16:20:58 +0700

    git commit

---
+++ CMakeFiles/nlopt.dir/src/api/deprecated.c.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/nlopt.dir/src/api/deprecated.c.o: \
+ /home/thanh/nlopt/src/api/deprecated.c /usr/include/stdc-predef.h \
+ /home/thanh/nlopt/src/api/nlopt.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stddef.h
